bfa4c35bce2485dabf836fd971bc54fd99910b15
liusu0325 <908148781@qq.com>
2019-07-24 00:05:34 +0800

    写了一些login.py

--- passcet/login.py
+++ passcet/login.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
-from untitled import settings
 from passcet import checkcode
+from passcet import settingfile as SF
 def login(request):
     token = request.POST.get('token')
     type = request.POST.get('type') #type 0：验证存在并发送验证码 type1：验证验证码并返回结果，同时把用户的ID返回给客户端
     phone = request.POST.get('phone')
     email = request.POST.get('email')
-    # if token != settings.PASSCET_TOKEN:
-
-    # return HttpResponse('success')
+    if token != SF.PASSCET_TOKEN and token != None:
+        if type != None and (phone!=None or email != None):
+            if type == 0:
+                print()
+            elif type == 1:
+                print()
+            else:
+                return HttpResponse(SF.PASSCET_PARAMETER_ERROR)
+        else:
+            return HttpResponse(SF.PASSCET_PARAMETER_ERROR)
+    else:
+        return HttpResponse(SF.PASSCET_TOKEN_ERROR)
