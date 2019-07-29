b38073f8468ec8f332bf01af96643b092f0fd202
liusu0325 <908148781@qq.com>
2019-07-29 23:44:49 +0800

    完成登录

--- passcet/login.py
+++ passcet/login.py
@@ -5,22 +5,29 @@ def login(request):
     type = request.POST.get('type') #type 0：验证存在并发送验证码 type1：验证验证码并返回结果，同时把用户的ID返回给客户端
     phone = request.POST.get('phone')
     email = request.POST.get('email')
+    id = request.POST.get('id')
+    code = request.POST.get('code')
     if token != SF.PASSCET_TOKEN and token != None:
         if type != None and (phone!=None or email != None):
             if type == 0: #用户是否存在
                 if phone == None:
                     # 处理邮件验证码
                     if len(models.passcet_user.objects.filter(email=email)) == 1:
-                        register.sendMail(email)
+                        return register.sendMail(email)
                     else:
                         return HttpResponse(SF.PASSCET_USER_DOES_NOT_EXIST)
                 else:
                     if len(models.passcet_user.objects.filter(phone=phone)) == 1:
-                        register.sendSMS(phone)
+                        return register.sendSMS(phone)
                     else:
                         return HttpResponse(SF.PASSCET_USER_DOES_NOT_EXIST)
             elif type == 1:
-                print()
+                if phone != None and code != None:
+                    checkcode.checkPhone(phone,code)
+                elif id != None and code != None:
+                    checkcode.chenkemail(id,code)
+                else:
+                    return HttpResponse(SF.PASSCET_PARAMETER_ERROR) #缺少参数
             else:
                 return HttpResponse(SF.PASSCET_PARAMETER_ERROR)
         else:
