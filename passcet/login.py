5d0aa2aed6d16ae730551565263dcca0155289e9
liusu0325 <908148781@qq.com>
2019-08-02 23:37:12 +0800

    明天我一定好好写代码。。

--- passcet/login.py
+++ passcet/login.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from passcet import checkcode,models,settingfile as SF,register
 def login(request):
     token = request.POST.get('token')
-    type = request.POST.get('type') #type 0：验证存在并发送验证码 type1：验证验证码并返回结果，同时把用户的ID返回给客户端
+    type = request.POST.get('type') #type 0：验证存在并发送验证码 type1：验证验证码并返回结果，同时把用户的ID返回给客户端。
     phone = request.POST.get('phone')
     email = request.POST.get('email')
     id = request.POST.get('id')
