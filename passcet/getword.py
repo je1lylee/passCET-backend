d39b37b96be60551c4affc36e8e979a2fcfb6a01
liusu0325 <908148781@qq.com>
2019-08-18 19:32:20 +0800

    梳理出Json字符串的信息

--- passcet/getword.py
+++ passcet/getword.py
@@ -8,6 +8,22 @@ from passcet import models
 # 直接请求上面儿的网址即可返回一个标准的json
 
 def getword(request):
+    testString = ''
     resource_json = requests.get('http://www.iciba.com/index.php?a=getWordMean&c=search&list=1%2C2%2C3%2C4%2C5%2C8%2C9%2C10%2C12%2C13%2C14%2C15%2C18%2C21%2C22%2C24%2C3003%2C3004%2C3005&word=ambition&_=1565436821302')
     json_res = json.loads(resource_json.text) #转换为dictionary
-    return HttpResponse(json_res['baesInfo']['word_name'])
+    print(json_res['baesInfo']['word_name'])
+    print(json_res['baesInfo']['symbols'][0]['ph_en'])
+    print(json_res['baesInfo']['symbols'][0]['ph_am'])
+    print(json_res['baesInfo']['symbols'][0]['ph_en_mp3'])
+    print(json_res['baesInfo']['symbols'][0]['ph_am_mp3'])
+    print(json_res['baesInfo']['symbols'][0]['parts'][0]) # 遍历List
+    for i in json_res['baesInfo']['symbols'][0]['parts']:
+        testString = testString+i
+        # print(i)  释义
+    print(testString)
+    print(json_res['sentence']) #例句
+    if 'cetFour' in json_res:
+        print(json_res['cetFour']['count'])
+    if 'cetSix' in json_res:
+        print(json_res['cetSix']['count'])
+    return HttpResponse(SF.PASSCET_101_OK)
--- passcet/settingfile.py
+++ passcet/settingfile.py
@@ -14,6 +14,7 @@ PASSCET_208_EMAIL_MESSAGE_ERROR = '{"code":208,"status":"邮箱验证码错误"}
 PASSCET_209_PHONE_MESSAGE_ID_ERROR = '{"code":209,"status":"邮箱验证码ID不存在"}'
 PASSCET_210_NO_IMG_DATA = '{"code":210,"status":"图片数据不存在"}'
 # 正确返回
+PASSCET_101_OK = '{"code":101,"status":"成功"}' #仅用作测试
 PASSCET_102_SEND_PHONE_MESSAGE_OK = '{"code":102,"status":"成功发送短信验证码"}'
 PASSCET_103_SEND_EMAIL_MESSAGE_OK = '{"code":103,"status":"成功发送邮箱验证码验证码","id":"'
 PASSCET_104_CHECK_PHONE_MESSAGE_OK = '{"code":104,"status":"短信验证码验证成功"}'
