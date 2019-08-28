09ebded75089f2d5c3ce5dacbf4801ca1b194c02
liusu0325 <908148781@qq.com>
2019-08-28 11:48:49 +0800

    添加读数据库并返回

--- passcet/getword.py
+++ passcet/getword.py
@@ -1,3 +1,5 @@
+import traceback
+from django.core import serializers
 from django.http import HttpResponse
 import requests
 import json
@@ -15,19 +17,23 @@ def getword(request):
     resource_json = requests.get(
         'http://www.iciba.com/index.php?a=getWordMean&c=search&list=1%2C2%2C3%2C4%2C5%2C8%2C9%2C10%2C12%2C13%2C14%2C15%2C18%2C21%2C22%2C24%2C3003%2C3004%2C3005&word='+QueryWord)
     json_res = json.loads(resource_json.text)  # 转换为dictionary
-    print(json_res['baesInfo']['word_name'])
-    print(json_res['baesInfo']['symbols'][0]['ph_en'])
-    print(json_res['baesInfo']['symbols'][0]['ph_am'])
-    print(json_res['baesInfo']['symbols'][0]['ph_en_mp3'])
-    print(json_res['baesInfo']['symbols'][0]['ph_am_mp3'])
-    # print(json_res['baesInfo']['symbols'][0]['parts'][0]) # 遍历List
-    demoString = json.dumps(json_res['baesInfo']['symbols'][0])
-    print('测试字符串' + demoString)
-    for i in json_res['baesInfo']['symbols'][0]['parts']:
-        testString = testString + json.dumps(i) + ','
-        # print(i)  释义
-    print(testString)
-    print(json.dumps(json_res['sentence']))  # 例句
+    try:
+        print(json_res['baesInfo']['word_name'])
+        print(json_res['baesInfo']['symbols'][0]['ph_en'])
+        print(json_res['baesInfo']['symbols'][0]['ph_am'])
+        print(json_res['baesInfo']['symbols'][0]['ph_en_mp3'])
+        print(json_res['baesInfo']['symbols'][0]['ph_am_mp3'])
+        # print(json_res['baesInfo']['symbols'][0]['parts'][0]) # 遍历List
+        demoString = json.dumps(json_res['baesInfo']['symbols'][0])
+        print('测试字符串' + demoString)
+        for i in json_res['baesInfo']['symbols'][0]['parts']:
+            testString = testString + json.dumps(i) + ','
+            # print(i)  释义
+        print(testString)
+        print(json.dumps(json_res['sentence']))  # 例句
+    except:
+        traceback.print_exc()
+        return HttpResponse(SF.PASSCET_211_WORD_ERROR)
     # 开始存储数据
     if models.passcet_word.objects.filter(word='"'+str(QueryWord)+'"').count() :
         models.passcet_word.objects.filter(word='"'+str(QueryWord)+'"').delete()
@@ -75,4 +81,5 @@ def getword(request):
     # 开始检索数据库
     QuerySett = models.passcet_word.objects.filter(word='"'+str(QueryWord)+'"')
     print(QuerySett)
+    return HttpResponse(json.dumps(list(QuerySett.values())))
     return HttpResponse(SF.PASSCET_101_OK)
