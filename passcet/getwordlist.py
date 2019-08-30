606c9969dc45e3aa3e5e336542842106580a7779
liusu0325 <908148781@qq.com>
2019-08-30 14:38:46 +0800

    完成获取生词表的接口

--- passcet/getwordlist.py
+++ passcet/getwordlist.py
@@ -1,5 +1,16 @@
 # 获取用户生词表
 from passcet import settingfile as SF
 from django.http import HttpResponse
+from passcet import models
+import json
 def getwordlist(request):
-    return HttpResponse(SF.PASSCET_101_OK)
+    token = request.POST.get('token')
+    userid = request.POST.get('userid')
+    if token != SF.PASSCET_TOKEN or token == None:
+        return HttpResponse(SF.PASSCET_201_TOKEN_ERROR)
+    elif userid == None:
+        return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)
+    else:
+        print('执行逻辑')
+        QuerySett = models.passcet_glossary.objects.filter(user_id=userid) #QuerySet
+        return HttpResponse(json.dumps(list(QuerySett.values())))
