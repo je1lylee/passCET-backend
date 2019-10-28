3549e5e72c1044107e0377b8acb323ffbd8cbb01
liusu0325 <908148781@qq.com>
2019-10-28 15:48:23 +0800

    格式化

--- passcet/bindaccount.py
+++ passcet/bindaccount.py
@@ -5,6 +5,8 @@ from passcet import models
 from passcet import register
 from passcet import checkcode
 from passcet.utils.takeLog import takelog
+
+
 # 检查账号是否存在，如果存在检查将要绑定的手机或邮箱是不是已经被绑定过了
 def bindaccount(request):
     token = request.POST.get('token')
@@ -16,52 +18,52 @@ def bindaccount(request):
     # 0：发送验证码 1：验证验证码
     id = request.POST.get('id')
     code = request.POST.get('code')
-    #验证TOKEN
+    # 验证TOKEN
     if token != None and token == SF.PASSCET_TOKEN:
         if type != None and (phone != None or email != None) and type != None and option != None:
-            if type == 0: # 0:已经绑定邮箱，需要绑定手机号
-                if len(models.passcet_user.objects.filter(email = email)) == 1: #查库，有这个账号
-                    if option == 0: # send code
+            if type == 0:  # 0:已经绑定邮箱，需要绑定手机号
+                if len(models.passcet_user.objects.filter(email=email)) == 1:  # 查库，有这个账号
+                    if option == 0:  # send code
                         return register.sendSMS(phone)
-                    elif option == 1: #check code
-                        codestatus_json = checkcode.checkPhone(phone,code) #检查短信的验证码是否正确
+                    elif option == 1:  # check code
+                        codestatus_json = checkcode.checkPhone(phone, code)  # 检查短信的验证码是否正确
                         codestatus = json.loads(codestatus_json.text)
                         if 'code' in codestatus:
-                            if(codestatus['code'] == '104'):
-                                models.passcet_user.objects.filter(email = email).update(phone=phone);
-                                takelog(__file__,SF.PASSCET_108_BIND_PHONE_SUCCESS)
+                            if (codestatus['code'] == '104'):
+                                models.passcet_user.objects.filter(email=email).update(phone=phone);
+                                takelog(__file__, SF.PASSCET_108_BIND_PHONE_SUCCESS)
                                 return HttpResponse(SF.PASSCET_108_BIND_PHONE_SUCCESS)
                             else:
                                 return HttpResponse(codestatus_json)
                     else:
-                        takelog(__file__,SF.PASSCET_202_PARAMETER_ERROR)
+                        takelog(__file__, SF.PASSCET_202_PARAMETER_ERROR)
                         return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)
                 else:
-                    takelog(__file__,SF.PASSCET_205_USER_DOES_NOT_EXIST)
+                    takelog(__file__, SF.PASSCET_205_USER_DOES_NOT_EXIST)
                     return HttpResponse(SF.PASSCET_205_USER_DOES_NOT_EXIST)
             elif type == 1:
                 if len(models.passcet_user.objects.filter(phone=phone)) == 1:
-                    if option ==0:
+                    if option == 0:
                         return register.sendMail(email)
                     elif option == 1:
-                        codestatus_json = checkcode.chenkemail(id,code)
+                        codestatus_json = checkcode.chenkemail(id, code)
                         codestatus = json.loads(codestatus_json.text)
                         if code in codestatus:
                             if codestatus['code'] == '105':
-                                models.passcet_user.objects.filter(phone=phone).update(email = email)
-                                takelog(__file__,SF.PASSCET_109_BIND_EMAIL_SUCCESS)
+                                models.passcet_user.objects.filter(phone=phone).update(email=email)
+                                takelog(__file__, SF.PASSCET_109_BIND_EMAIL_SUCCESS)
                                 return HttpResponse(SF.PASSCET_109_BIND_EMAIL_SUCCESS)
                             else:
                                 return HttpResponse(codestatus_json)
                     else:
-                        takelog(__file__,SF.PASSCET_202_PARAMETER_ERROR)
+                        takelog(__file__, SF.PASSCET_202_PARAMETER_ERROR)
                         return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)
             else:
-                takelog(__file__,SF.PASSCET_202_PARAMETER_ERROR)
+                takelog(__file__, SF.PASSCET_202_PARAMETER_ERROR)
                 return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)
         else:
-            takelog(__file__,SF.PASSCET_202_PARAMETER_ERROR)
+            takelog(__file__, SF.PASSCET_202_PARAMETER_ERROR)
             return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)
     else:
-        takelog(__file__,SF.PASSCET_201_TOKEN_ERROR)
+        takelog(__file__, SF.PASSCET_201_TOKEN_ERROR)
         return HttpResponse(SF.PASSCET_201_TOKEN_ERROR)
