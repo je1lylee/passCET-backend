c6140c8f6a36cae1f9f29eb87e31f15cafd88d3e
liusu0325 <908148781@qq.com>
2019-11-04 15:19:01 +0800

    update login.py

--- passcet/login.py
+++ passcet/login.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from passcet import checkcode, models, settingfile as SF, register
 from passcet.utils.takeLog import takelog
-from passcet.utils import verifiyMailCode,verifiySmsCode,sendSms,sendMail
+from passcet.utils import sendMail, sendSms, verifiySmsCode, verifiyMailCode
 
 
 def login(request):
@@ -11,31 +11,31 @@ def login(request):
     email = request.POST.get('email')
     id = request.POST.get('id')
     code = request.POST.get('code')
-    if token == SF.PASSCET_TOKEN and token != None:
-        if type != None:
-            if type == '0' and (phone != None or email != None):  # 用户是否存在
-                if phone == None:
+    if token == SF.PASSCET_TOKEN and token is not None:
+        if type is not None:
+            if type == '0' and (phone is not None or email is not None):  # 用户是否存在
+                if phone is None:
                     # 处理邮件验证码
                     if len(models.passcet_user.objects.filter(email=email)) == 1:
                         takelog(__file__, SF.PASSCET_101_OK)
-                        return register.sendMail(email)
+                        return sendMail.sendMail(email)
                     else:
                         takelog(__file__, SF.PASSCET_205_USER_DOES_NOT_EXIST)
                         return HttpResponse(SF.PASSCET_205_USER_DOES_NOT_EXIST)
                 else:
                     if len(models.passcet_user.objects.filter(phone=phone)) == 1:
                         takelog(__file__, SF.PASSCET_101_OK)
-                        return register.sendSMS(phone)
+                        return sendSms.sendSMS(phone)
                     else:
                         takelog(__file__, SF.PASSCET_205_USER_DOES_NOT_EXIST)
                         return HttpResponse(SF.PASSCET_205_USER_DOES_NOT_EXIST)
             elif type == '1':
-                if phone != None and code != None:
+                if phone is not None and code is not None:
                     takelog(__file__, SF.PASSCET_101_OK)
-                    return checkcode.checkPhone(phone, code)
-                elif id != None and code != None:
+                    return verifiySmsCode.verifiySmsCode(phone, code)
+                elif id is not None and code is not None:
                     takelog(__file__, SF.PASSCET_101_OK)
-                    return checkcode.chenkemail(id, code)
+                    return verifiyMailCode.verifiyMailCode(id, code)
                 else:
                     takelog(__file__, SF.PASSCET_202_PARAMETER_ERROR)
                     return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)  # 缺少参数
