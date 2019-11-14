d726a1fe4ed28a39e57fbfed7343844dfc1bad6c
liusu0325 <908148781@qq.com>
2019-11-14 17:40:06 +0800

    添加检查版本的部分代码

--- passcet/system/checkversion.py
+++ passcet/system/checkversion.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 from passcet.utils import *
 from passcet import settingfile as SF
+
+
 def checkversion(request):
+    """
+    检查应用程序的版本
+    :param request:
+    :return:如果版本为最新返回对应的返回码。
+    如果需要更新则返回对应的返回码，
+    """
+    token = request.POST.get('token')
+    version = request.POST.get('version')
     pass
