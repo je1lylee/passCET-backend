c0e53debf6b596f1e59a712a82359b491f7e7743
liusu0325 <908148781@qq.com>
2019-11-15 08:37:27 +0800

    完成版本检查接口

--- passcet/settingfile.py
+++ passcet/settingfile.py
@@ -1,6 +1,8 @@
 # Powered By Aquarius Engine /Ver BETA/ include Backend AI IoT
-# 参数设置
+# TOKEN设置
 PASSCET_TOKEN = 'SMvwlN1kjrtKzIfxCLHlejDedpVSTRvW'
+# 程序版本设置
+PASSCET_VERSION = '1.0'
 # 管理员邮箱设置
 PASSCET_ADMIN_EMAIL_LIGUODONG = '908148781@qq.com'
 PASSCET_ADMIN_EMAIL_LIANJIE = '3042143991@qq.com'
@@ -21,6 +23,7 @@ PASSCET_213_DB_ERROR = '{"code":213,"status":"数据库异常"}'
 PASSCET_214_WORD_EUPLICATE = '{"code":214,"status":"用户生词本中已存在该单词"}'
 PASSCET_215_GLOSSARY_NO_DATA = '{"code":215,"status":"用户生词本中不存在该单词"}'
 PASSCET_216_FEED_BACK_EAMIL_SEND_ERROR = '{"code":216,"status":"反馈邮件发送失败"}'
+PASSCET_217_VERSION_CHECK_FAILED = '{"code":217,"status":"当前版本不是最新版本"}'
 # 正确返回
 PASSCET_101_OK = '{"code":101,"status":"成功"}'  # 仅用作测试
 PASSCET_102_SEND_PHONE_MESSAGE_OK = '{"code":102,"status":"成功发送短信验证码"}'
@@ -34,3 +37,4 @@ PASSCET_109_BIND_EMAIL_SUCCESS = '{"code":109,"status":"邮箱绑定成功"}'
 PASSCET_110_ADD_GLOSSARY_SUCCESS = '{"code":110,"status":"单词已添加到生词本"}'
 PASSCET_111_DEL_GLOSSARY_SUCCESS = '{"code":110,"status":"单词已从生词本中移除"}'
 PASSCET_112_FEED_BACK_EMAIL_SEND_SUCCESS = '{"code":112,"status":"反馈邮件发送成功"}'
+PASSCET_113_VERSION_CHECK_OK = '{"code":113,"status":"当前版本为最新版本"}'
--- passcet/system/checkversion.py
+++ passcet/system/checkversion.py
@@ -12,4 +12,15 @@ def checkversion(request):
     """
     token = request.POST.get('token')
     version = request.POST.get('version')
+    if token is not None and token == SF.PASSCET_TOKEN:
+        if version is not None:
+            #开始执行逻辑
+            if version == SF.PASSCET_VERSION:
+                return HttpResponse(SF.PASSCET_113_VERSION_CHECK_OK)
+            else:
+                return HttpResponse(SF.PASSCET_217_VERSION_CHECK_FAILED)
+        else:
+            return HttpResponse(SF.PASSCET_202_PARAMETER_ERROR)
+    else:
+        return HttpResponse(SF.PASSCET_201_TOKEN_ERROR)
     pass
